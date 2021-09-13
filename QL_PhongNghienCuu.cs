c1221a93ca8f9b75cce0e5fefc932163f61ccd3e
dangharold <90547689+dangharold@users.noreply.github.com>
2021-09-13 22:21:17 +0700

    Create QL_PhongNghienCuu.cs

---
+++ QL_PhongNghienCuu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ql_pnc
+{
+    public abstract class NhaKhoaHoc : NhanVienCapCao
+    {
+        private int sobai;      
+        public int Sobai { get => sobai; set => sobai = value; }
+        public NhaKhoaHoc(string fullname,int dob,string chucvu, int bacluong,int songaycong, int sobai) : base(fullname,dob,chucvu,bacluong,songaycong)
+        {
+            this.sobai = sobai;
+        }
+
+        public override void Input()
+        {
+            base.Input();
+            Console.WriteLine("so bai bao cao da cong bo: ");
+            sobai = Console.Read();
+        }
+        public override void Output()
+        {
+            base.Output();
+            Console.WriteLine("so bai bao cao da cong bo: "+sobai); 
+
+        }
+        
+    }
+}
